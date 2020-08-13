b53b4f153e7a9cc57550d92cb9ab507ed181ef8e
Yo! <yo@pop-os.localdomain>
2020-08-13 13:45:55 +0300

    Navbar fix

--- src/components/elements/navbar/Navbar.js
+++ src/components/elements/navbar/Navbar.js
@@ -3,15 +3,16 @@ import React from 'react'
 import "./navbar-styles.scss"
 const Navbar = () => {
     let currentURL =window.location.href;
-    let localHost ="http://localhost:3000/"
+    let localHost ="http://localhost:3000/";
+    let herokuHost = "https://radiant-brushlands-91450.herokuapp.com/"
     return (
     <div className="navbar" >
             <ul className="navbar-ul">
                 {console.log(currentURL)}
                 <li style={{
-                     backgroundColor: currentURL  == localHost ? "#A259FF":"white",
-                     color:currentURL  == localHost ?  "white":"#A259FF",
-                     paddingTop:currentURL==localHost?"20px":"0px"
+                     backgroundColor: currentURL  == localHost || currentURL  == herokuHost ? "#A259FF":"white",
+                     color:currentURL  == localHost || currentURL  == herokuHost  ?  "white":"#A259FF",
+                     paddingTop:currentURL==localHost || currentURL  == herokuHost  ?"20px":"0px"
                 }}><a href="/">Home</a></li>
 
 
